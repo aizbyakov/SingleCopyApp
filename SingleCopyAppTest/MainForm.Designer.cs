7078f133a08837d967b8c69a2b678a236dad9c9d
Andrei Izbiakov <andrei.izbiakov@pctel.com>
2020-09-06 13:48:16 -0400

    Add another modal form for tests

--- SingleCopyAppTest/MainForm.Designer.cs
+++ SingleCopyAppTest/MainForm.Designer.cs
@@ -28,13 +28,37 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnOpenAnotherForm = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnOpenAnotherForm
+            // 
+            this.btnOpenAnotherForm.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnOpenAnotherForm.Location = new System.Drawing.Point(302, 191);
+            this.btnOpenAnotherForm.Name = "btnOpenAnotherForm";
+            this.btnOpenAnotherForm.Size = new System.Drawing.Size(199, 66);
+            this.btnOpenAnotherForm.TabIndex = 0;
+            this.btnOpenAnotherForm.Text = "Open another form";
+            this.btnOpenAnotherForm.UseVisualStyleBackColor = true;
+            this.btnOpenAnotherForm.Click += new System.EventHandler(this.btnOpenAnotherForm_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnOpenAnotherForm);
+            this.Name = "MainForm";
             this.Text = "Main Form";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnOpenAnotherForm;
     }
 }
 
--- SingleCopyAppTest/MainForm.cs
+++ SingleCopyAppTest/MainForm.cs
@@ -4,9 +4,17 @@ namespace SingleCopyAppTest
 {
     public partial class MainForm : Form
     {
+        private AnotherForm anotherForm;
+
         public MainForm()
         {
             InitializeComponent();
+        }
+
+        private void btnOpenAnotherForm_Click(object sender, System.EventArgs e)
+        {
+            anotherForm = new AnotherForm();
+            anotherForm.ShowDialog();
         }
     }
 }
